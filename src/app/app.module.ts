717c571a2abf7227f181de0c54ac491ae7e72be1
gastongst <gastongst@gmail.com>
2023-08-06 19:01:19 -0300

    paso 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
+import { BlogComponent } from './componentes/blog/blog.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { EquipoComponent } from './componentes/equipo/equipo.component';
+import { TestimoniosComponent } from './componentes/testimonios/testimonios.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { PiepaginaComponent } from './componentes/piepagina/piepagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    InicioComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    BlogComponent,
+    ClientesComponent,
+    EquipoComponent,
+    TestimoniosComponent,
+    ContactoComponent,
+    PiepaginaComponent
   ],
   imports: [
     BrowserModule
